0314f7e8959a03542ba5153be3b3ae9f25a0f943
Danyil Fiut <danyilfiut@gmail.com>
2024-03-07 20:57:45 +0100

    buttons for pick difficult active

--- app/src/main/java/com/example/coloredbottles/DifficultActivity.kt
+++ app/src/main/java/com/example/coloredbottles/DifficultActivity.kt
@@ -1,12 +1,36 @@
 package com.example.coloredbottles
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class DifficultActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_difficult_select)
+
+        val easyButton = findViewById<LinearLayout>(R.id.easyGame)
+        val medium = findViewById<LinearLayout>(R.id.mediumGame)
+        val hard = findViewById<LinearLayout>(R.id.hardGame)
+
+        val onClickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.easyGame -> startGameActivity("easy")
+                R.id.mediumGame -> startGameActivity("medium")
+                R.id.hardGame-> startGameActivity("hard")
+            }
+        }
+        easyButton.setOnClickListener(onClickListener)
+        medium.setOnClickListener(onClickListener)
+        hard.setOnClickListener(onClickListener)
+    }
+    private fun startGameActivity(difficulty: String){
+        val intent = Intent(this, GameActivity::class.java)
+        intent.putExtra("difficulty", difficulty)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/coloredbottles/GameActivity.kt
@@ -0,0 +1,18 @@
+package com.example.coloredbottles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val layoutId = when (intent.getStringExtra("difficulty")){
+            "easy" -> R.layout.activity_esay_game
+            "medium" -> R.layout.activity_medium_game
+            "hard" -> R.layout.activity_hard_game
+            else -> R.layout.activity_main_menu
+        }
+        setContentView(layoutId)
+    }
+}
