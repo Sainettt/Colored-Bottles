67cb038d966fb73950bbc561d55af6d55461aca4
Danyil Fiut <danyilfiut@gmail.com>
2024-03-07 19:36:11 +0100

    button start is Active

--- app/src/main/java/com/example/coloredbottles/MainActivity.kt
+++ app/src/main/java/com/example/coloredbottles/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.coloredbottles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,9 +9,10 @@ import android.widget.LinearLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_medium_game)
+        setContentView(R.layout.activity_main_menu)
     }
-    fun onButtonClick(view: View){
-
+    fun onStartButtonClick(view: View) {
+        val intent = Intent(this, DifficultActivity::class.java)
+        startActivity(intent)
     }
 }
